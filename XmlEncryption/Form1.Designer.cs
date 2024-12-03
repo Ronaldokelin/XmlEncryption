33242950efabfc64711a1224f49a403a80ffcdeb
Ronaldokelin <90939300+Ronaldokelin@users.noreply.github.com>
2024-12-03 11:02:16 -0300

    first code

--- XmlEncryption/Form1.Designer.cs
+++ XmlEncryption/Form1.Designer.cs
@@ -29,13 +29,77 @@ namespace XmlEncryption
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.btnDecryptFile = new System.Windows.Forms.Button();
+            this.btnEncryptFile = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(289, 12);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(200, 20);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Selecione Arquivo";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // btnDecryptFile
+            // 
+            this.btnDecryptFile.Location = new System.Drawing.Point(12, 58);
+            this.btnDecryptFile.Name = "btnDecryptFile";
+            this.btnDecryptFile.Size = new System.Drawing.Size(230, 60);
+            this.btnDecryptFile.TabIndex = 1;
+            this.btnDecryptFile.Text = "Descriptografar";
+            this.btnDecryptFile.UseVisualStyleBackColor = true;
+            this.btnDecryptFile.Click += new System.EventHandler(this.btnDecryptFile_Click);
+            // 
+            // btnEncryptFile
+            // 
+            this.btnEncryptFile.Location = new System.Drawing.Point(259, 58);
+            this.btnEncryptFile.Name = "btnEncryptFile";
+            this.btnEncryptFile.Size = new System.Drawing.Size(230, 60);
+            this.btnEncryptFile.TabIndex = 2;
+            this.btnEncryptFile.Text = "Encriptografar";
+            this.btnEncryptFile.UseVisualStyleBackColor = true;
+            this.btnEncryptFile.Click += new System.EventHandler(this.btnEncryptFile_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(12, 12);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(270, 20);
+            this.txtFilePath.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(501, 139);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnEncryptFile);
+            this.Controls.Add(this.btnDecryptFile);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "Form1";
+            this.Text = "XmlEncryptior";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Button btnDecryptFile;
+        private System.Windows.Forms.Button btnEncryptFile;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- XmlEncryption/Form1.cs
+++ XmlEncryption/Form1.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace XmlEncryption
 {
@@ -15,6 +12,121 @@ namespace XmlEncryption
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Arquivos XML|*.xml";
+            openFileDialog1.Title = "Selecione um Arquivo XML";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnEncryptFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txtFilePath.Text) && File.Exists(txtFilePath.Text))
+                {
+                    string xmlData = File.ReadAllText(txtFilePath.Text);
+                    string encryptionKey = "Al1FGHyEWTeK6PNfyWf1qeM6ePznQG6K"; // Chave original 32 Bytes
+                    string encryptedXml = Encrypt(xmlData, encryptionKey, true); // useHashing = true
+                    File.WriteAllText(txtFilePath.Text, encryptedXml);
+                    MessageBox.Show("Arquivo criptografado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, selecione um arquivo XML válido.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao criptografar o arquivo: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDecryptFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txtFilePath.Text) && File.Exists(txtFilePath.Text))
+                {
+                    string encryptedXml = File.ReadAllText(txtFilePath.Text);
+                    string encryptionKey = "Al1FGHyEWTeK6PNfyWf1qeM6ePznQG6K"; // Chave original
+                    string decryptedXml = Decrypt(encryptedXml, encryptionKey, true); // useHashing = true
+                    File.WriteAllText(txtFilePath.Text, decryptedXml);
+                    MessageBox.Show("Arquivo descriptografado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, selecione um arquivo XML válido.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao descriptografar o arquivo: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static string Encrypt(string toEncrypt, string key, bool useHashing)
+        {
+            byte[] keyArray;
+            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(toEncrypt);
+
+            if (useHashing)
+            {
+                using (MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider())
+                {
+                    keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                }
+            }
+            else
+            {
+                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+            }
+
+            using (TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider())
+            {
+                tdes.Key = keyArray;
+                tdes.Mode = CipherMode.ECB;
+                tdes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform cTransform = tdes.CreateEncryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+                return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+            }
+        }
+
+        public static string Decrypt(string toDecrypt, string key, bool useHashing)
+        {
+            byte[] keyArray;
+            byte[] toDecryptArray = Convert.FromBase64String(toDecrypt);
+
+            if (useHashing)
+            {
+                using (MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider())
+                {
+                    keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                }
+            }
+            else
+            {
+                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+            }
+
+            using (TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider())
+            {
+                tdes.Key = keyArray;
+                tdes.Mode = CipherMode.ECB;
+                tdes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform cTransform = tdes.CreateDecryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toDecryptArray, 0, toDecryptArray.Length);
+                return UTF8Encoding.UTF8.GetString(resultArray);
+            }
         }
     }
 }
